95a5ca68eaebcb39e7a0a7de892ac4487d12b2fd
Nguyễn Đăng Bảo Long <longndb@slitigenz.io>
2025-03-13 12:21:47 +0700

    Include file for full 12 svc istio deployed rules, include rule delay for 2 svc, include k6 benchmark

---
+++ benchmark/k6/k6-load-test.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '10s', target: 10 }, // Ramp up to 10 users
+        { duration: '30s', target: 10 }, // Stay at 10 users
+        { duration: '10s', target: 0 },  // Ramp down to 0 users
+    ],
+};
+
+export default function () {
+    http.get('http://192.168.49.2:32385/');
+    sleep(1);
+}
